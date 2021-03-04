975e5ee61dab460ef9fc89417f31fe90e00d3582
santiago pc <santiagopereyra.95@gmail.com>
2021-03-04 02:56:39 -0300

    add readme, licenseInFiles and screenshot

---
+++ README.md
@@ -0,0 +1,1 @@
+![alt text](https://github.com/asp95/a9os_app_holamundo/blob/master/screenshot.png?raw=true)
--- html/php/a9os/app/holamundo/main.php
+++ html/php/a9os/app/holamundo/main.php
@@ -1,4 +1,20 @@
 <?php 
+/*_gtlsc_
+ * Holamundo for os.com.ar (a9os)
+ * Copyright (C) 2021  Santiago Pereyra (asp95)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 
 class a9os_app_holamundo_main extends a9os_core_window{
 	public function main($postData){
